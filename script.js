c990c8bca624e13eaaad095fde6dc88315e6efb2
CourierTracker <toppmanking@gmail.com>
2025-11-18 18:45:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ trackingForm.addEventListener('submit', function(e) {
       showError("❌ Invalid tracking number! Please try again.");
     }
 
-  }, 500); // spinner initial delay
+  }, 5000); // spinner initial delay
 });
 
 // -------------------------
